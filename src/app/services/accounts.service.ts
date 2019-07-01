169394f41603c9518ec3717d09b2185ebc8b0458
Chenguang Wang <wcg19920705@gmail.com>
2019-07-01 15:04:32 -0400

    0701 updates

---
+++ src/app/services/accounts.service.ts
@@ -0,0 +1,33 @@
+
+import { Injectable } from "@angular/core";
+import { LoggingService } from "./logging.service";
+
+@Injectable() // This informs angular that this is a receiving service( can inject things to this), only add this if you intend to inject something here
+export class AccountsService {
+    accounts = [
+        {
+            name: 'Master Account',
+            status: 'active'
+        },
+        {
+            name: 'Test Account',
+            status: 'inactive'
+        },
+        {
+            name: 'Hidden Account',
+            status: 'unknown'
+        }
+    ];
+
+    constructor(private loggingService: LoggingService) {}
+
+    addAccount(name: string, status: string) {
+        this.accounts.push({name: name, status: status});
+        this.loggingService.logStatusChange(status);
+    }
+
+    updateStatus(id: number, status: string) {
+        this.accounts[id].status = status;
+        this.loggingService.logStatusChange(status);
+    }
+}
